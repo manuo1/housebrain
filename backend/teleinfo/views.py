e04faa9684b43507ac449a2ecc3865d5e820229b
manuo1 <manuo1@free.fr>
2025-05-15 17:24:17 +0200

    feat(api): add alphabetical sorting to Teleinfo view response

--- backend/teleinfo/views.py
+++ backend/teleinfo/views.py
@@ -4,5 +4,9 @@ from django.http import JsonResponse
 
 def teleinfo_data(request):
     """Return the latest Teleinfo data as JSON."""
+
+
+def teleinfo_view(request):
     data = cache.get("teleinfo_data", {})
-    return JsonResponse(data)
+    sorted_data = dict(sorted(data.items()))
+    return JsonResponse(sorted_data)

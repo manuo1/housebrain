cf5adabf1298066a0c366723f85f2f5011ebe5b3
manuo1 <manuo1@free.fr>
2025-05-17 19:18:07 +0200

    feat(sensors): Add BluetoothListener to systemd

--- backend/sensors/views.py
+++ backend/sensors/views.py
@@ -4,6 +4,6 @@ from django.http import JsonResponse
 
 def sensors_data(request):
     """Return the latest sensors data as JSON."""
-    data = cache.get("sensors_data", {"created": None, "last_saved_at": None})
+    data = cache.get("sensors_data", {"no": "data"})
     sorted_data = dict(sorted(data.items()))
     return JsonResponse(sorted_data)

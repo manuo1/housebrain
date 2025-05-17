9b9ee23129eac17e80837cf3632b839327650fde
manuo1 <manuo1@free.fr>
2025-05-17 17:20:05 +0200

    feat(sensors): create sensors app and add basic view to display sensor data

---
+++ backend/sensors/views.py
@@ -0,0 +1,9 @@
+from django.core.cache import cache
+from django.http import JsonResponse
+
+
+def sensors_data(request):
+    """Return the latest sensors data as JSON."""
+    data = cache.get("sensors_data", {"created": None, "last_saved_at": None})
+    sorted_data = dict(sorted(data.items()))
+    return JsonResponse(sorted_data)

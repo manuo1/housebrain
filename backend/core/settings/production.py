a29afdaf17afa6bfeef073dc98f9c06b96940bbe
manuo1 <manuo1@free.fr>
2025-05-18 10:40:10 +0200

    chore(deployement): add and use raspberry public IP addresses

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -4,10 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-RASPBERRYPI_LOCAL_IP = os.environ["RASPBERRYPI_LOCAL_IP"]
+RASPBERRYPI_LOCAL_IP = os.environ.get("RASPBERRYPI_LOCAL_IP")
+RASPBERRYPI_PUBLIC_IP = os.environ.get("RASPBERRYPI_PUBLIC_IP")
 
 DEBUG = False
-ALLOWED_HOSTS = ["housebrain", "127.0.0.1", "localhost", RASPBERRYPI_LOCAL_IP]
+ALLOWED_HOSTS = ["housebrain", "127.0.0.1", "localhost"]
+
+if RASPBERRYPI_LOCAL_IP:
+    ALLOWED_HOSTS.append(RASPBERRYPI_LOCAL_IP)
+
+if RASPBERRYPI_PUBLIC_IP:
+    ALLOWED_HOSTS.append(RASPBERRYPI_PUBLIC_IP)
+
 
 CACHES = {
     "default": {

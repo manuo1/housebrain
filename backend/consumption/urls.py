a8a075f81330d0a83c01a6b62476d9bcfbcd91b7
manuo1 <manuo1@free.fr>
2025-06-14 18:41:37 +0200

    feat(consumption/api): add django rest framework and API endpoint to get daily watt-hour consumption

--- backend/consumption/urls.py
+++ backend/consumption/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("indexes/<str:date>/", views.indexes, name="indexes"),
+    path("api/", include("consumption.api.urls")),
 ]

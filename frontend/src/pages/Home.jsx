3e758218fcbec50a14adfe99862adde32bb057ef
manuo1 <manuo1@free.fr>
2025-06-15 12:54:02 +0200

    feat(frontend): add initial React setup with dependencies

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+      <h1>HouseBrain</h1>
+    </div>
+  );
+}

652500eb6a05a1bb6db11bdbd1f1f79f7bcd4392
manuo1 <manuo1@free.fr>
2025-06-18 20:16:31 +0200

    feat(models): add tarif_periods field to DailyIndexes model

--- backend/consumption/models.py
+++ backend/consumption/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class DailyIndexes(models.Model):
     date = models.DateField(unique=True)
     values = models.JSONField(default=dict)
+    tarif_periods = models.JSONField(default=dict)
 
     def __str__(self):
         return f"Indexes du {self.date}"

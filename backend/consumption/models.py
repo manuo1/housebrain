fa7dc6cd38461369b063b08eaf86a98841e1e9f6
manuo1 <manuo1@free.fr>
2025-06-18 21:22:56 +0200

    feat(mutators): store subscribed power in DailyIndexes.tarif_periods

--- backend/consumption/models.py
+++ backend/consumption/models.py
@@ -5,6 +5,7 @@ class DailyIndexes(models.Model):
     date = models.DateField(unique=True)
     values = models.JSONField(default=dict)
     tarif_periods = models.JSONField(default=dict)
+    subscribed_power = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return f"Indexes du {self.date}"

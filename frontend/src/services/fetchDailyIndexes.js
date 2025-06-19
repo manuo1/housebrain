e85bcad5e07d887107f1f8e679ecd48d58538233
manuo1 <manuo1@free.fr>
2025-06-19 23:04:18 +0200

    refactor(api): update consumption endpoint path and frontend fetch URL

--- frontend/src/services/fetchDailyIndexes.js
+++ frontend/src/services/fetchDailyIndexes.js
@@ -2,7 +2,7 @@ import { fetchJson } from "./fetchJson";
 import DailyIndexes from "../models/DailyIndexes";
 
 async function fetchDailyIndexes(date) {
-  const data = await fetchJson(`/api/consumption/wh/${date}/`);
+  const data = await fetchJson(`/api/consumption/${date}/`);
   return new DailyIndexes(data.date, data.watt_hours, data.totals);
 }
 

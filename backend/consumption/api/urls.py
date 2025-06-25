db988b593748e904238e4927e283d224385eef34
manuo1 <manuo1@free.fr>
2025-06-25 21:10:06 +0200

    refactor(api): replace URL param with query param for daily consumption route

--- backend/consumption/api/urls.py
+++ backend/consumption/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import DailyConsumptionView
 
 urlpatterns = [
-    path("<str:date>/", DailyConsumptionView.as_view(), name="daily-consumption"),
+    path("daily/", DailyConsumptionView.as_view(), name="daily-consumption"),
 ]

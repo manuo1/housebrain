712dd149bb65fc315938d0f039531956e97196d4
manuo1 <manuo1@free.fr>
2025-06-27 07:44:19 +0200

    refactor: move related components into element-based folders

---
+++ frontend/src/constants/consumptionConstants.js
@@ -0,0 +1,8 @@
+export const STEP_OPTIONS = [
+  { value: 1, label: "1 min" },
+  { value: 30, label: "30 min" },
+  { value: 60, label: "1 h" },
+  { value: "day", label: "Jour" },
+  { value: "week", label: "Semaine" },
+  { value: "month", label: "Mois" },
+];

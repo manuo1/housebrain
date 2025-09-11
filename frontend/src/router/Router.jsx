17f2f71db87edf542dd4964a18d0974cbe13040f
manuo1 <manuo1@free.fr>
2025-09-11 16:24:25 +0200

    feat: integrate Layout in Router

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -3,11 +3,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../pages/Home";
 import Consumption from "../pages/Consumption";
 import LiveTeleinfoPage from "../pages/LiveTeleinfoPage";
+import Layout from "../layouts/Layout";
+import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/consumption", element: <Consumption /> },
-  { path: "/teleinfo", element: <LiveTeleinfoPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "consumption", element: <Consumption /> },
+      { path: "teleinfo", element: <LiveTeleinfoPage /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
 ]);
 
 export default function AppRouter() {

06d50a7d73268c4ce8f104a074930bac9fd26321
manuo1 <manuo1@free.fr>
2025-10-03 21:35:25 +0200

    feat(scheduler): add radiator sync to periodic tasks

--- backend/scheduler/management/commands/periodic_tasks.py
+++ backend/scheduler/management/commands/periodic_tasks.py
@@ -1,6 +1,7 @@
 import logging
 from django.core.management.base import BaseCommand
 
+from actuators.services.radiator_synchronization import RadiatorSyncService
 from consumption.mutators import save_teleinfo_data
 
 logger = logging.getLogger("django")
@@ -11,3 +12,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         save_teleinfo_data()
+        RadiatorSyncService.synchronize_database_and_hardware()

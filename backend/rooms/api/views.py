da0819a2d0efab1542df284662aecb5b95432bf3
manuo1 <manuo1@free.fr>
2025-10-16 10:39:40 +0200

    feat(rooms-api): update RoomListView to use real data instead of fake data

--- backend/rooms/api/views.py
+++ backend/rooms/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rooms.api.selectors import get_rooms_data_for_api
 from rooms.api.serializers import RoomOutputSerializer
+from rooms.api.services import (
+    add_temperature_measurements_to_rooms,
+    transform_room_data_for_api,
+)
+from sensors.utils.cache_sensors_data import get_sensors_data_in_cache
 
 
 class RoomListView(APIView):
@@ -13,55 +19,13 @@ class RoomListView(APIView):
     """
 
     def get(self, request):
-        # FAKE DATA
-        fake_data = [
-            {
-                "id": 1,
-                "name": "Salon",
-                "heating": {"mode": "thermostat", "value": 21.5},
-                "temperature": {
-                    "id": 1,
-                    "mac_short": "46:C0:F4",
-                    "signal_strength": 4,
-                    "measurements": {"temperature": 21.79, "trend": "up"},
-                },
-                "radiator": {"id": 1, "state": "on"},
-            },
-            {
-                "id": 2,
-                "name": "Chambre",
-                "heating": {"mode": "on_off", "value": "on"},
-                "temperature": {
-                    "id": 2,
-                    "mac_short": "B2:1F:44",
-                    "signal_strength": 3,
-                    "measurements": {"temperature": 22.28, "trend": "same"},
-                },
-                "radiator": {"id": 2, "state": "load_shed"},
-            },
-            {
-                "id": 3,
-                "name": "Bureau",
-                "heating": {"mode": "thermostat", "value": 20.0},
-                "temperature": None,
-                "radiator": {"id": 3, "state": "off"},
-            },
-            {
-                "id": 4,
-                "name": "Garage",
-                "heating": {"mode": "on_off", "value": "off"},
-                "temperature": {
-                    "id": 4,
-                    "mac_short": "4C:64:60",
-                    "signal_strength": 1,
-                    "measurements": {
-                        "temperature": None,
-                        "trend": None,
-                    },
-                },
-                "radiator": None,
-            },
-        ]
+        rooms_data = []
+        rooms = get_rooms_data_for_api()
+        sensors_cache = get_sensors_data_in_cache()
+        add_temperature_measurements_to_rooms(rooms, sensors_cache)
 
-        serializer = RoomOutputSerializer(fake_data, many=True)
+        for room in rooms:
+            rooms_data.append(transform_room_data_for_api(room))
+
+        serializer = RoomOutputSerializer(rooms_data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)

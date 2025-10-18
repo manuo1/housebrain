5e971637c25d7653d9bc8d86de11fc1357279242
manuo1 <manuo1@free.fr>
2025-10-18 16:33:14 +0200

    feat(teleinfo): add cache_teleinfo_data utils

---
+++ backend/teleinfo/utils/cache_teleinfo_data.py
@@ -0,0 +1,24 @@
+from core.utils.date_utils import is_delta_within_five_seconds, parse_iso_datetime
+from core.utils.env_utils import environment_is_development
+from django.core.cache import cache
+from django.utils import timezone
+from mock_data.teleinfo import MOCKED_TELEINFO_DATA
+
+
+def get_teleinfo_data_in_cache() -> dict:
+    if environment_is_development():
+        return MOCKED_TELEINFO_DATA
+    return cache.get("teleinfo_data", {"last_read": None})
+
+
+def get_teleinfo_data_in_cache_if_up_to_date() -> dict | None:
+    data = get_teleinfo_data_in_cache()
+    if environment_is_development():
+        MOCKED_TELEINFO_DATA["last_read"] = timezone.now().isoformat()
+    last_read = parse_iso_datetime(data.get("last_read"))
+    if last_read and is_delta_within_five_seconds(last_read, timezone.now()):
+        return data
+
+
+def set_teleinfo_data_in_cache(data: dict) -> None:
+    cache.set("teleinfo_data", data, timeout=None)

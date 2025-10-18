5c39d6c68043060e2e431cccd08a909d254bf0df
manuo1 <manuo1@free.fr>
2025-10-18 17:58:37 +0200

    feat(teleinfo): add function to get available power from teleinfo in cache

--- backend/teleinfo/tests/test_cache_teleinfo_data.py
+++ backend/teleinfo/tests/test_cache_teleinfo_data.py
@@ -1,11 +1,13 @@
 from datetime import timedelta
 
 import pytest
+from core.constants import DEFAULT_VOLTAGE
 from django.core.cache import cache
 from django.utils import timezone
 from freezegun import freeze_time
 from mock_data.teleinfo import MOCKED_TELEINFO_DATA
 from teleinfo.utils.cache_teleinfo_data import (
+    get_instant_available_power,
     get_teleinfo_data_in_cache,
     get_teleinfo_data_in_cache_if_up_to_date,
     set_teleinfo_data_in_cache,
@@ -93,3 +95,32 @@ def test_set_teleinfo_data_in_cache():
     cache.set("teleinfo_data", {}, timeout=None)
     set_teleinfo_data_in_cache(UP_TO_DATE_CACHE_DATA)
     assert cache.get("teleinfo_data") == UP_TO_DATE_CACHE_DATA
+
+
+@pytest.mark.parametrize(
+    "cache, excepted",
+    [
+        ({"ISOUSC": "45", "IINST": "3"}, (45 - 3) * DEFAULT_VOLTAGE),
+        # No power left
+        ({"ISOUSC": "45", "IINST": "45"}, 0),
+        # Exceeded power
+        ({"ISOUSC": "45", "IINST": "50"}, 0),
+        # Missing required field
+        ({"NOTUSED": "45", "IINST": "50"}, None),
+        ({"ISOUSC": "45", "NOTUSED": "50"}, None),
+        ({"NOTUSED": "50"}, None),
+        ({}, None),
+        # Unexpected values
+        ({"ISOUSC": None, "IINST": "3"}, None),
+        ({"ISOUSC": [], "IINST": "50"}, None),
+        ({"ISOUSC": "a", "IINST": "50"}, None),
+        ({"ISOUSC": {}, "IINST": "50"}, None),
+    ],
+)
+def test_get_instant_available_power(monkeypatch, cache, excepted):
+    monkeypatch.setattr(
+        "teleinfo.utils.cache_teleinfo_data.get_teleinfo_data_in_cache_if_up_to_date",
+        lambda: cache,
+    )
+
+    assert get_instant_available_power() == excepted

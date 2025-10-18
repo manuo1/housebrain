6e06af8189d10806c6dab164944b825048b447b8
manuo1 <manuo1@free.fr>
2025-10-19 00:37:54 +0200

    feat(radiators): add get_radiators_data_for_load_shedding selector with tests

--- backend/actuators/selectors/radiators.py
+++ backend/actuators/selectors/radiators.py
@@ -4,11 +4,6 @@ from actuators.models import Radiator
 def get_radiators_state_in_database() -> list[dict]:
     """
     Return the minimum info needed for driver synchronization:
-    - id (int)
-    - control_pin (int)
-    - requested_state (str)
-    - actual_state (str)
-    - error (str | None)
     """
     return list(
         Radiator.objects.values(
@@ -18,4 +13,15 @@ def get_radiators_state_in_database() -> list[dict]:
             "actual_state",
             "error",
         )
+    )
+
+
+def get_radiators_data_for_load_shedding() -> list[dict]:
+    """
+    Return the minimum info needed for load shedding
+    """
+    return list(
+        Radiator.objects.filter(actual_state=Radiator.ActualState.ON, power__gt=0)
+        .order_by("-importance", "-power")
+        .values("id", "power", "importance")
     )

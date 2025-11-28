1a9e0350a190d5b989fa0ad437a5e37c4788b494
manuo1 <manuo1@free.fr>
2025-11-28 17:47:09 +0100

    feat(heating) Add slot value to requested heating state mapping

--- backend/heating/mappers.py
+++ backend/heating/mappers.py
@@ -20,3 +20,15 @@ def radiator_state_matches_room_state(
             return radiator_requested_state is None
         case _:
             return False
+
+
+def heating_pattern_slot_value_to_room_requested_heating_state(
+    slot_value: str,
+) -> Room.RequestedHeatingState:
+    if not isinstance(slot_value, str):
+        return Room.RequestedHeatingState.UNKNOWN
+    mapping = {
+        "on": Room.RequestedHeatingState.ON,
+        "off": Room.RequestedHeatingState.OFF,
+    }
+    return mapping.get(slot_value, Room.RequestedHeatingState.UNKNOWN)

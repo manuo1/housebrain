bf6e32d1be56f948f23e338a1a122ce4ef7efab2
manuo1 <manuo1@free.fr>
2025-12-05 15:51:40 +0100

    refactor(sensors): Increase get_sensor_temperatures

--- backend/sensors/tests/test_sensors_services_temperatures.py
+++ backend/sensors/tests/test_sensors_services_temperatures.py
@@ -1,10 +1,11 @@
-from datetime import datetime, timezone
 from unittest.mock import patch
 
 import pytest
+from freezegun import freeze_time
 from sensors.services.temperatures import get_sensor_temperatures
 
 
+@freeze_time("2025-10-15T13:00:00Z")
 def test_get_sensor_temperatures_real_case():
     sensor_data_in_cache = {
         "mac_address": "38:1F:8D:65:E9:1C",
@@ -31,21 +32,12 @@ def test_get_sensor_temperatures_real_case():
         ),
     ):
         assert get_sensor_temperatures("38:1F:8D:65:E9:1C") == {
-            "temperature": 20.69,
-            "dt": datetime(2025, 10, 15, 12, 59, 32, 465000, tzinfo=timezone.utc),
-            "previous_temperature": 20.68,
-            "previous_dt": datetime(
-                2025, 10, 15, 12, 58, 37, 566000, tzinfo=timezone.utc
-            ),
+            "current": 20.69,
+            "previous": 20.68,
         }
 
 
-DEFAULT_SENSOR_TEMPERATURES = {
-    "temperature": None,
-    "dt": None,
-    "previous_temperature": None,
-    "previous_dt": None,
-}
+DEFAULT_SENSOR_TEMPERATURES = {"current": None, "previous": None}
 
 
 @pytest.mark.parametrize(
@@ -54,60 +46,71 @@ DEFAULT_SENSOR_TEMPERATURES = {
         ([], DEFAULT_SENSOR_TEMPERATURES),
         ("a", DEFAULT_SENSOR_TEMPERATURES),
         ({}, DEFAULT_SENSOR_TEMPERATURES),
+        # measurements missing
         (
             {
                 "previous_measurements": {
-                    "temperature": 1,
-                    "dt": "2025-10-15T00:00:00Z",
+                    "temperature": 2,
+                    "dt": "2025-10-15T12:58:30Z",
                 },
             },
-            DEFAULT_SENSOR_TEMPERATURES,
+            {"current": None, "previous": 2.0},
         ),
+        # previous_measurements missing
         (
             {
                 "measurements": {
                     "temperature": 1,
-                    "dt": "2025-10-15T00:00:00Z",
+                    "dt": "2025-10-15T12:59:30Z",
                 },
             },
-            DEFAULT_SENSOR_TEMPERATURES,
+            {"current": 1, "previous": None},
         ),
+        # missing dt field
         (
             {
                 "measurements": {
                     "temperature": 1,
-                    "dt": "2025-10-15T00:00:00Z",
+                    "dt": "2025-10-15T12:59:30Z",
                 },
                 "previous_measurements": {
                     "temperature": 1,
                 },
             },
-            DEFAULT_SENSOR_TEMPERATURES,
+            {"current": 1, "previous": None},
         ),
+        # missing temperature field
         (
             {
                 "measurements": {
                     "temperature": 1,
-                    "dt": "2025-10-15T00:00:00Z",
+                    "dt": "2025-10-15T12:59:30Z",
                 },
                 "previous_measurements": {
-                    "dt": "2025-10-15T00:00:00Z",
+                    "dt": "2025-10-15T12:58:30Z",
                 },
             },
-            DEFAULT_SENSOR_TEMPERATURES,
+            {"current": 1, "previous": None},
         ),
+        # Outdated dt
         (
             {
                 "measurements": {
                     "temperature": 1,
-                    "dt": "2025-10-15T00:00:00Z",
+                    "dt": "2025-10-15T12:59:30Z",
                 },
-                "previous_measurements": {},
+                "previous_measurements": {
+                    "previous_measurements": {
+                        "temperature": 2,
+                        "dt": "2025-10-15T12:57:30Z",  # <-
+                    },
+                },
             },
-            DEFAULT_SENSOR_TEMPERATURES,
+            {"current": 1, "previous": None},
         ),
     ],
 )
+@freeze_time("2025-10-15T13:00:00Z")
 def test_get_sensor_temperatures_other_case(sensor_data_in_cache, excepted):
     with (
         patch(
@@ -115,5 +118,4 @@ def test_get_sensor_temperatures_other_case(sensor_data_in_cache, excepted):
             return_value=sensor_data_in_cache,
         ),
     ):
-        print(get_sensor_temperatures("mac"))
         assert get_sensor_temperatures("mac") == excepted

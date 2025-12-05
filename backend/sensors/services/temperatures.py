db6a6d1423ef134ecf2519b76f5fb81d51644b68
manuo1 <manuo1@free.fr>
2025-12-05 15:51:40 +0100

    refactor(sensors): Increase get_sensor_temperatures

--- backend/sensors/services/temperatures.py
+++ backend/sensors/services/temperatures.py
@@ -1,28 +1,34 @@
-from core.utils.date_utils import parse_iso_datetime
+from core.utils.date_utils import (
+    is_delta_within_one_minute,
+    is_delta_within_two_minute,
+    parse_iso_datetime,
+)
 from core.utils.temperatures import validate_temperature_value
+from django.utils import timezone
 from sensors.utils.cache_sensors_data import get_sensor_data_in_cache
 
 
 def get_sensor_temperatures(mac_address: str) -> dict:
     sensor_info = get_sensor_data_in_cache(mac_address)
-
+    now = timezone.localtime(timezone.now())
+    temperatures = {"current": None, "previous": None}
     try:
-        return {
-            "temperature": validate_temperature_value(
+        dt = parse_iso_datetime(sensor_info["measurements"]["dt"])
+        temperature_is_recent = is_delta_within_one_minute(dt, now)
+        if temperature_is_recent:
+            temperatures["current"] = validate_temperature_value(
                 sensor_info["measurements"]["temperature"]
-            ),
-            "dt": parse_iso_datetime(sensor_info["measurements"]["dt"]),
-            "previous_temperature": validate_temperature_value(
-                sensor_info["previous_measurements"]["temperature"]
-            ),
-            "previous_dt": parse_iso_datetime(
-                sensor_info["previous_measurements"]["dt"]
-            ),
-        }
+            )
     except (TypeError, KeyError):
-        return {
-            "temperature": None,
-            "dt": None,
-            "previous_temperature": None,
-            "previous_dt": None,
-        }
+        pass
+    try:
+        previous_dt = parse_iso_datetime(sensor_info["previous_measurements"]["dt"])
+        temperature_is_recent = is_delta_within_two_minute(previous_dt, now)
+        if temperature_is_recent:
+            temperatures["previous"] = validate_temperature_value(
+                sensor_info["previous_measurements"]["temperature"]
+            )
+    except (TypeError, KeyError):
+        pass
+
+    return temperatures

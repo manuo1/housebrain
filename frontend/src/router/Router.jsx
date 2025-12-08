9a7de4efbbe8c90d0b72563d2c7338ef45b4c448
manuo1 <manuo1@free.fr>
2025-12-08 21:31:45 +0100

    feat: add heating schedule route and navigation

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,20 +1,22 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "../pages/Home";
-import Consumption from "../pages/Consumption";
-import LiveTeleinfoPage from "../pages/LiveTeleinfoPage";
-import Layout from "../layouts/Layout";
-import NotFound from "../pages/NotFound";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../pages/Home';
+import Consumption from '../pages/Consumption';
+import LiveTeleinfoPage from '../pages/LiveTeleinfoPage';
+import HeatingSchedulePage from '../pages/HeatingSchedulePage';
+import Layout from '../layouts/Layout';
+import NotFound from '../pages/NotFound';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-      { path: "consumption", element: <Consumption /> },
-      { path: "teleinfo", element: <LiveTeleinfoPage /> },
-      { path: "*", element: <NotFound /> },
+      { path: 'consumption', element: <Consumption /> },
+      { path: 'teleinfo', element: <LiveTeleinfoPage /> },
+      { path: 'heating/schedule', element: <HeatingSchedulePage /> },
+      { path: '*', element: <NotFound /> },
     ],
   },
 ]);

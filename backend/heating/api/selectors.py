5ea03b6f49e2301a1d0bb49749f27d5c395ca355
manuo1 <manuo1@free.fr>
2025-12-09 20:10:27 +0100

    feat(heating/api) add get_daily_heating_plan selector

--- backend/heating/api/selectors.py
+++ backend/heating/api/selectors.py
@@ -1,6 +1,11 @@
 from datetime import date
 
+from django.db.models import F, JSONField, OuterRef, Subquery, Value
+
+# from django.db.models.fields.json import JSONField
+from django.db.models.functions import Coalesce
 from heating.models import RoomHeatingDayPlan
+from rooms.models import Room
 
 
 def get_slots_hashes(start_date: date, end_date: date) -> list:
@@ -11,3 +16,26 @@ def get_slots_hashes(start_date: date, end_date: date) -> list:
             date__gte=start_date, date__lte=end_date
         ).values_list("date", "heating_pattern__slots_hash")
     )
+
+
+def get_daily_heating_plan(day: date) -> list:
+    if not isinstance(day, date):
+        return []
+
+    slots_subquery = RoomHeatingDayPlan.objects.filter(
+        room_id=OuterRef("id"), date=day
+    ).values("heating_pattern__slots")[:1]
+
+    qs = (
+        Room.objects.annotate(
+            room_id=F("id"),
+            slots=Coalesce(
+                Subquery(slots_subquery, output_field=JSONField()),
+                Value([], output_field=JSONField()),
+            ),
+        )
+        .order_by("name", "room_id")
+        .values("room_id", "name", "slots")
+    )
+
+    return list(qs)
--- backend/heating/tests/test_api_selectors.py
+++ backend/heating/tests/test_api_selectors.py
@@ -1,7 +1,7 @@
-from datetime import date
+from datetime import date, timedelta
 
 import pytest
-from heating.api.selectors import get_slots_hashes
+from heating.api.selectors import get_daily_heating_plan, get_slots_hashes
 from heating.tests.factories import HeatingPatternFactory, RoomHeatingDayPlanFactory
 from rooms.tests.factories import RoomFactory
 
@@ -65,3 +65,61 @@ def test_get_slots_hashes_no_heating():
 def test_get_slots_hashes_incorrect_dates(start_date, end_date):
     result = get_slots_hashes(start_date, end_date)
     assert result == []
+
+
+@pytest.mark.django_db
+def test_get_daily_heating_plan_normal_cases():
+    room_1 = RoomFactory(id=1)  # room with Heating Day Plan in selected day
+    room_2 = RoomFactory(id=2)  # room with Heating Day Plan in selected day
+    room_3 = RoomFactory(id=3)  # room with Heating Day Plan not in selected day
+    room_4 = RoomFactory(id=4)  # room without Heating Day Plan
+
+    pattern_1 = [{"start": "07:00", "end": "23:30", "type": "onoff", "value": "on"}]
+    pattern_2 = [{"start": "12:00", "end": "23:30", "type": "onoff", "value": "on"}]
+
+    heating_pattern_1 = HeatingPatternFactory(slots=pattern_1)
+    heating_pattern_2 = HeatingPatternFactory(slots=pattern_2)
+
+    # Heating Day Plan in selected date
+    RoomHeatingDayPlanFactory(
+        room=room_1,
+        date=DEFAULT_DATE,
+        heating_pattern=heating_pattern_1,
+    )
+    # Heating Day Plan in selected date
+    RoomHeatingDayPlanFactory(
+        room=room_2,
+        date=DEFAULT_DATE,
+        heating_pattern=heating_pattern_2,
+    )
+    # Heating Day Plan out of selected date
+    RoomHeatingDayPlanFactory(
+        room=room_3,
+        date=DEFAULT_DATE + timedelta(days=1),
+        heating_pattern=heating_pattern_1,
+    )
+
+    assert get_daily_heating_plan(DEFAULT_DATE) == [
+        {
+            "room_id": 1,
+            "name": "Room0",
+            "slots": [
+                {"start": "07:00", "end": "23:30", "type": "onoff", "value": "on"}
+            ],
+        },
+        {
+            "room_id": 2,
+            "name": "Room1",
+            "slots": [
+                {"start": "12:00", "end": "23:30", "type": "onoff", "value": "on"}
+            ],
+        },
+        {"room_id": 3, "name": "Room2", "slots": []},
+        {"room_id": 4, "name": "Room3", "slots": []},
+    ]
+
+
+@pytest.mark.parametrize("day", [None, False, [], {}, "a"])
+@pytest.mark.django_db
+def test_get_daily_heating_plan_incorrect_cases(day):
+    assert get_daily_heating_plan(day) == []

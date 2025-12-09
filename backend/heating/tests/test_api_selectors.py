bf0fac7bc18282f2169c16ff52c58cd759f469c4
manuo1 <manuo1@free.fr>
2025-12-09 20:24:27 +0100

    refactor(heating/api) filter daily heating plan to rooms with radiators only

--- backend/heating/tests/test_api_selectors.py
+++ backend/heating/tests/test_api_selectors.py
@@ -1,6 +1,7 @@
 from datetime import date, timedelta
 
 import pytest
+from actuators.tests.factories import RadiatorFactory
 from heating.api.selectors import get_daily_heating_plan, get_slots_hashes
 from heating.tests.factories import HeatingPatternFactory, RoomHeatingDayPlanFactory
 from rooms.tests.factories import RoomFactory
@@ -69,10 +70,16 @@ def test_get_slots_hashes_incorrect_dates(start_date, end_date):
 
 @pytest.mark.django_db
 def test_get_daily_heating_plan_normal_cases():
-    room_1 = RoomFactory(id=1)  # room with Heating Day Plan in selected day
-    room_2 = RoomFactory(id=2)  # room with Heating Day Plan in selected day
-    room_3 = RoomFactory(id=3)  # room with Heating Day Plan not in selected day
-    room_4 = RoomFactory(id=4)  # room without Heating Day Plan
+    # room with Heating Day Plan in selected day
+    room_1 = RoomFactory(id=1, name="room_1", radiator=RadiatorFactory(id=1))
+    # room with Heating Day Plan in selected day
+    room_2 = RoomFactory(id=2, name="room_2", radiator=RadiatorFactory(id=2))
+    # room with Heating Day Plan not in selected day
+    room_3 = RoomFactory(id=3, name="room_3", radiator=RadiatorFactory(id=3))
+    # room without Heating Day Plan
+    room_4 = RoomFactory(id=4, name="room_4", radiator=RadiatorFactory(id=4))
+    # room without radiator
+    room_5 = RoomFactory(id=5, name="room_5")
 
     pattern_1 = [{"start": "07:00", "end": "23:30", "type": "onoff", "value": "on"}]
     pattern_2 = [{"start": "12:00", "end": "23:30", "type": "onoff", "value": "on"}]
@@ -98,24 +105,25 @@ def test_get_daily_heating_plan_normal_cases():
         date=DEFAULT_DATE + timedelta(days=1),
         heating_pattern=heating_pattern_1,
     )
+    print(get_daily_heating_plan(DEFAULT_DATE))
 
     assert get_daily_heating_plan(DEFAULT_DATE) == [
         {
             "room_id": 1,
-            "name": "Room0",
+            "name": "room_1",
             "slots": [
                 {"start": "07:00", "end": "23:30", "type": "onoff", "value": "on"}
             ],
         },
         {
             "room_id": 2,
-            "name": "Room1",
+            "name": "room_2",
             "slots": [
                 {"start": "12:00", "end": "23:30", "type": "onoff", "value": "on"}
             ],
         },
-        {"room_id": 3, "name": "Room2", "slots": []},
-        {"room_id": 4, "name": "Room3", "slots": []},
+        {"room_id": 3, "name": "room_3", "slots": []},
+        {"room_id": 4, "name": "room_4", "slots": []},
     ]
 
 

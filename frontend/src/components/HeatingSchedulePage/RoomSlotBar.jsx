c29bce22af5c526bce4a5ec4d3e94bcdd47b7e20
manuo1 <manuo1@free.fr>
2025-12-09 10:02:25 +0100

    feat: add RoomSlotBar component, contain SlotBarLabel and SlotBar

---
+++ frontend/src/components/HeatingSchedulePage/RoomSlotBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SlotBarLabel from './SlotBarLabel';
+import SlotBar from './SlotBar';
+import styles from './RoomSlotBar.module.scss';
+
+export default function RoomSlotBar({ room }) {
+  return (
+    <div className={styles.roomSlotBar}>
+      <SlotBarLabel roomName={room.name} />
+      <SlotBar slots={room.slots} mode={room.mode} />
+    </div>
+  );
+}

9453ae694357e82739a12ce4a9acd77397e7baa9
manuo1 <manuo1@free.fr>
2025-12-11 00:46:06 +0100

    feat(front/HeatingSchedulePage): add slot editing with validation and delete functionality

--- frontend/src/components/HeatingSchedulePage/RoomSlotBar.jsx
+++ frontend/src/components/HeatingSchedulePage/RoomSlotBar.jsx
@@ -3,11 +3,17 @@ import SlotBarLabel from './SlotBarLabel';
 import SlotBar from './SlotBar';
 import styles from './RoomSlotBar.module.scss';
 
-export default function RoomSlotBar({ room }) {
+export default function RoomSlotBar({ room, onSlotClick }) {
+  const handleSlotClick = (slotData, slotIndex) => {
+    if (onSlotClick) {
+      onSlotClick(room, slotData, slotIndex);
+    }
+  };
+
   return (
     <div className={styles.roomSlotBar}>
       <SlotBarLabel roomName={room.name} />
-      <SlotBar slots={room.slots} />
+      <SlotBar slots={room.slots} onSlotClick={handleSlotClick} />
     </div>
   );
 }

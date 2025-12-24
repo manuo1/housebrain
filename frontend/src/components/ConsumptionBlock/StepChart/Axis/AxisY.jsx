650f03d861f9612c38c8fad9b8392f5b6ab8bad8
manuo1 <manuo1@free.fr>
2025-12-24 16:29:05 +0100

    refactor(front/new consumption graph): Small ui adujstments for Y axis labels

--- frontend/src/components/ConsumptionBlock/StepChart/Axis/AxisY.jsx
+++ frontend/src/components/ConsumptionBlock/StepChart/Axis/AxisY.jsx
@@ -3,7 +3,6 @@ import styles from './AxisY.module.scss';
 
 const AxisY = ({ labels }) => {
   const reversedLabels = [...labels].reverse();
-  reversedLabels[reversedLabels.length - 1] = '';
   return (
     <div className={styles.axisY}>
       {reversedLabels.map((label, index) => (

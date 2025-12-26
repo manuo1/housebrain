1dd90a9dc22b53b408f9ca5fd16c223ac1ac2e44
manuo1 <manuo1@free.fr>
2025-12-26 20:38:13 +0100

    refactor: replace Recharts with custom CSS/SVG PowerGauge

---
+++ frontend/src/components/LiveTeleinfoPage/PowerGauge/PowerGauge.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import GaugeArc from './GaugeArc/GaugeArc';
+import GaugeNeedle from './GaugeNeedle/GaugeNeedle';
+import GaugeCenterText from './GaugeCenterText/GaugeCenterText';
+import GaugeTicks from './GaugeTicks/GaugeTicks';
+import styles from './PowerGauge.module.scss';
+
+export default function PowerGauge({ maxPower, currentPower }) {
+  if (!maxPower || currentPower === undefined) {
+    maxPower = 1;
+    currentPower = 0;
+  }
+
+  const percent = (currentPower / maxPower) * 100;
+
+  return (
+    <div className={styles.powerGauge}>
+      <div className={styles.gaugeContainer}>
+        {/* SVG de l'aiguille (derrière) */}
+        <svg viewBox="0 0 100 60" className={styles.svg}>
+          <GaugeNeedle percent={percent} />
+        </svg>
+
+        {/* SVG de l'arc + texte + graduations (devant) */}
+        <svg viewBox="0 0 100 60" className={styles.svg}>
+          <GaugeArc percent={percent} />
+          <GaugeCenterText currentPower={currentPower} percent={percent} />
+          <GaugeTicks maxPower={maxPower} />
+        </svg>
+      </div>
+    </div>
+  );
+}

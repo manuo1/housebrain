231a76bcd236eb3f3b61bf10b8d87ba57f3f75e1
manuo1 <manuo1@free.fr>
2026-07-29 09:24:21 +0200

    chore: remove unused dev mock system (USE_MOCK) from services

--- frontend/src/services/fetchDailyConsumption.ts
+++ frontend/src/services/fetchDailyConsumption.ts
@@ -1,23 +1,5 @@
 import { fetchJson } from "./fetchJson";
 import DailyConsumption, { DailyConsumptionElementRaw, TotalByLabelRaw } from "../models/DailyConsumption";
-import { mockDailyConsumption1 } from "../mocks/mockDailyConsumption1";
-import { mockDailyConsumption30 } from "../mocks/mockDailyConsumption30";
-import { mockDailyConsumption60 } from "../mocks/mockDailyConsumption60";
-
-const USE_MOCK = false;
-
-interface MockData {
-  date: string;
-  step: number;
-  data: DailyConsumptionElementRaw[];
-  totals: Record<string, TotalByLabelRaw>;
-}
-
-const MOCKS_BY_STEP: Record<number, MockData> = {
-  1: mockDailyConsumption1,
-  30: mockDailyConsumption30,
-  60: mockDailyConsumption60,
-};
 
 interface DailyConsumptionResponse {
   date: string;
@@ -27,14 +9,6 @@ interface DailyConsumptionResponse {
 }
 
 async function fetchDailyConsumption(date: string, step: number = 1): Promise<DailyConsumption> {
-  if (USE_MOCK) {
-    const mockData = MOCKS_BY_STEP[step];
-    if (!mockData) {
-      throw new Error(`No mock data available for step: ${step}`);
-    }
-    return new DailyConsumption(mockData.date, mockData.step, mockData.data, mockData.totals);
-  }
-
   if (!DailyConsumption.ALLOWED_STEPS.includes(step)) {
     throw new Error(`Invalid step value: ${step}. Allowed values: ${DailyConsumption.ALLOWED_STEPS.join(", ")}`);
   }
